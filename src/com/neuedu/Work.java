e712d1ba4eaa447acb3e609d6e6e142d855a4239
qi2715944686qi <2715944686@qq.com>
2019-05-27 20:08:44 +0800

    ddhgfhhgdc

---
+++ src/com/neuedu/Work.java
@@ -0,0 +1,25 @@
+package com.neuedu;
+
+public class Work {
+//    public void changfangxing1(){
+        //无参
+//        int width=1;
+//        int height=1;
+//        System.out.println("周长为："+(width+height)*2);
+//        System.out.println("面积为："+width*height);
+//        //一个参数
+//        public void changfangxing2(int a){
+//            int  width=a;
+//            int  height=a;
+//            System.out.println("周长为："+(width+height)*2);
+//            System.out.println("面积为："+width*height);
+//    }
+          //两个参数
+          public  void    changfangxing3(int  b,int   c){
+          int  width=b;
+          int  height=c;
+          System.out.println("周长为："+(width+height)*2);
+          System.out.println("面积为："+width*height);
+      }
+}
+
---
+++ src/com/neuedu/Work2.java
@@ -0,0 +1,19 @@
+package com.neuedu;
+
+public class Work2 {
+    public static void main(String[] args) {
+        //长方形无参
+//        System.out.println("无参的长方形");
+//        Work wucan = new Work();
+//        wucan.changfangxing1();
+         //长方形一个参数
+//        System.out.println("一个参数的长方形");
+//        Work yigecanshu=new Work();
+//        yigecanshu.changfangxing2(4);
+         //长方形两个参数
+          System.out.println("两个参的长方形");
+          Work liagecanshu=new Work();
+          liagecanshu.changfangxing3(5,6);
+    }
+}
+
